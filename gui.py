ab4d9c7344499524e09d0b203b1e3a3725a11ea6
M4dPac <pacc8891@gmail.com>
2024-09-03 10:06:57 +0300

    перенес настрйки элементов в классы

--- gui.py
+++ gui.py
@@ -10,9 +10,9 @@ config = {
         "height": 1080,
         "mon": 0,
     },
-    "shutdown_pc": False,
     "fishing_screen": {"x1": 666, "x2": 1248, "y1": 316, "y2": 419},
     "fishing_filter": {"hsv_min": [0, 33, 135], "hsv_max": [90, 255, 255]},
+    "shutdown_pc": False,
 }
 
 
@@ -22,20 +22,40 @@ class Settings:
             setattr(self, name, value)
 
 
+class TextField(ft.TextField):
+    def __init__(self, label, value):
+        super().__init__()
+        self.label = label
+        self.value = value
+        self.expand = True
+
+
+class ContainerIndigo(ft.Container):
+    def __init__(self, content):
+        super().__init__()
+        self.content = content
+        self.alignment = ft.alignment.center
+        self.bgcolor = ft.colors.INDIGO_500
+        self.margin = 10
+        self.padding = 10
+        self.border_radius = 10
+
+
 def main(page: ft.Page):
     page.title = "TLFishing"
     # page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    # Разрешение экрана
     screen_text = ft.Text("Разрешение экрана", size=20)
-    screen_width_textfield = ft.TextField(
-        label="Ширина", expand=True, value=str(settings.monitor_resolution["width"])
+    screen_width_textfield = TextField(
+        label="Ширина", value=str(settings.monitor_resolution["width"])
     )
-    screen_height_textfield = ft.TextField(
-        label="Высота", expand=True, value=str(settings.monitor_resolution["height"])
+    screen_height_textfield = TextField(
+        label="Высота", value=str(settings.monitor_resolution["height"])
     )
-    screen_number = ft.TextField(
-        label="Номер", expand=True, value=str(settings.monitor_resolution["mon"])
+    screen_number = TextField(
+        label="Номер Монитора", value=str(settings.monitor_resolution["mon"])
     )
     screen_textgield_row = ft.Row(
         controls=[screen_width_textfield, screen_height_textfield, screen_number]
@@ -44,13 +64,8 @@ def main(page: ft.Page):
     screen_column = ft.Column(
         controls=[screen_text, screen_textgield_row],
     )
-    screen_box = ft.Container(
+    screen_box = ContainerIndigo(
         content=screen_column,
-        alignment=ft.alignment.center,
-        bgcolor=ft.colors.INDIGO_500,
-        margin=10,
-        padding=10,
-        border_radius=10,
     )
 
     page.add(screen_box)
